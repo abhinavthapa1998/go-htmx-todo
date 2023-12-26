1574cb76e65aae18d13c823ef778de5a9b75d324
Abhinav Thapa <abhinavthapa1998@gmail.com>
2023-12-26 18:19:54 +0530

    init

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, _ *http.Request) {
+		w.Write([]byte("Hello, welcome to Go+HTMX"))
+	})
+	http.ListenAndServe("localhost:8080", r)
+}
